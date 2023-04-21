15b6c706eca86f3ab69176901de007a4022c9072
dawid-polak <dawidow.polak@gmail.com>
2023-04-22 00:32:46 +0200

    add basket component

---
+++ src/components/elements/Basket.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="main flex fixed h-full w-full bg-black bg-opacity-75 z-50 top-0">
+          <div class="basket fixed flex flex-col justify-between bg-white h-full w-11/12 sm:w-8/12 lg:w-5/12 z-40">
+               <div class="w-full flex justify-end">
+                    <img @click="$emit('close')" class="m-8 w-12 h-12 cursor-pointer hover:bg-zinc-200 p-2 rounded-full" src="../../assets/icons/close-dark.png" alt="close" />
+               </div>
+               <div class="flex-col w-full h-[400px] sm:h-[600px] items-center overflow-y-scroll border-y">
+
+               </div>
+               <div class="w-full flex my-10">
+                    <div class="w-1/2 h-full flex flex-col items-center justify-center">
+                         <p class="font-light">łączna cena: </p>
+                         <p class="font-thin text-xl">zł</p>
+                    </div>
+                    <div class="w-1/2 h-full flex flex-col items-center justify-center">
+                         <Button  :text="'Zapłać'"/>
+                    </div>
+               </div>
+          </div>
+     </div>
+</template>
+
+<script setup lang="ts">
+import Button from './Button.vue';
+</script>
+
+<style scoped lang="scss">
+.main {
+     opacity: 0;
+     animation: showMain 0.5s forwards ease-in-out;
+     .basket {
+          left: -100%;
+          animation: showBasket 0.5s forwards ease-in-out;
+
+          @keyframes showBasket {
+               from {
+                    left: -100%;
+               }
+
+               to {
+                    left: 0;
+               }
+          }
+     }
+
+     @keyframes showMain {
+          from {
+               opacity: 0;
+          }
+          to {
+               opacity: 1;
+          }
+     }
+}
+
+</style>
--- src/components/navigation/Navbar.vue
+++ src/components/navigation/Navbar.vue
@@ -1,18 +1,21 @@
 <template>
     <div class="w-full flex justify-between h-24 items-center fixed z-50 bg-white">
         <div class="ml-10">
-            <img class="w-12 h-12 cursor-pointer hover:shadow-xl rounded-full p-1" src="../../assets/icons/shopping-bag.png" alt="shopping-bag">
+            <img @click="showBasket = !showBasket" class="w-12 h-12 cursor-pointer hover:shadow-xl rounded-full p-1" src="../../assets/icons/shopping-bag.png" alt="shopping-bag">
         </div>
         <div class="mr-10">
             <Button :text="'Twoje Zamówienia'" />
         </div>
     </div>
+    <Basket v-if="showBasket" @close="showBasket = !showBasket"/>
 </template>
 
 <script setup lang="ts">
-
+import { ref } from 'vue';
 import Button from '../elements/Button.vue';
+import Basket from '../elements/Basket.vue';
 
+const showBasket = ref(false);
 
 </script>
 
