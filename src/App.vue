a98f7e66e59dece18ec44d2829ddb62be44e7eee
dawid-polak <dawidow.polak@gmail.com>
2023-04-22 00:58:36 +0200

    add backHome component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
      <Navbar />
      <router-view></router-view>
+     <BackHome />
 </template>
 
 <script setup lang="ts">
 import Navbar from "./components/navigation/Navbar.vue";
+import BackHome from "./components/elements/BackHome.vue";
 </script>
 
 <style scoped></style>
---
+++ src/components/elements/BackHome.vue
@@ -0,0 +1,16 @@
+<template>
+    <router-link :to="{ name: 'home' }">
+    
+        <div class="fixed z-50 mt-[60%] sm:mt-[30%] lg:mt-[15%] right-0 bg-black">
+            <img class="w-8 p-2" src="../../assets/icons/home.png" alt="home">
+        </div>
+    </router-link>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
